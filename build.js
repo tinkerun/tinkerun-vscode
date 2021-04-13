54663ec71416958ab513481f29a5415382c27027
billyct <billy_allen@126.com>
2021-04-13 23:54:41 +0800

    🐰 init commit

---
+++ build.js
@@ -0,0 +1,16 @@
+const { build } = require('esbuild')
+
+build({
+  entryPoints: [
+    './src/extension.ts'
+  ],
+  platform: 'node',
+  external: ['vscode'],
+  outdir: 'build',
+  tsconfig: './tsconfig.json',
+  bundle: true,
+  watch: true,
+  ...(process.env.NODE_ENV === 'production' ? {
+    watch: false
+  } : {})
+})
